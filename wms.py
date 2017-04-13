a1e9d494cfb4d956f19cbadf84aff458408702e0
Yaswant Narayan <kk.yaswantnarayan2016@vitstudent.ac.in>
2017-04-13 12:39:27 +0530

    Add files via upload

---
+++ wms.py
@@ -0,0 +1,20 @@
+import Adafruit_DHT
+from time import sleep
+import urllib2
+#import Adafruit_BMP.BMP085 as BMP085
+
+while True:
+      hum, temp = Adafruit_DHT.read_retry(11, 14)#DHT_11->11 and 8th pin BCM14
+      #sensor = BMP085.BMP085()
+      #prsr=sensor.read_pressure()
+      print("hum: "+str(hum)+"  temp: "+str(temp))#+"  prsr: "+str(prsr))
+      try:
+          url="http://api.thingspeak.com/update?api_key=5GZS39W3ZZH6Z2ZF&field1="+str(temp)+"&field2="+str(hum)+"&field3=100"
+          f=urllib2.urlopen(url)
+          print(f.read())
+          f.close()
+      except:
+            print("error uploading data")
+            pass
+      sleep(10)
+ 
