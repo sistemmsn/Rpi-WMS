7c5a9e53d90790414e010401eb011af805123763
Yaswant Narayan <kk.yaswantnarayan2016@vitstudent.ac.in>
2017-04-20 21:50:32 +0530

    Update wms.py

--- wms.py
+++ wms.py
@@ -8,14 +8,14 @@ while True:
             hum, temp = Adafruit_DHT.read_retry(11, 14)#DHT_11->11 and 8th pin BCM14
             sensor = BMP085.BMP085()
             prsr=sensor.read_pressure()
-            print("hum: "+str(hum)+"  temp: "+str(temp))#+"  prsr: "+str(prsr))
+            print("Humidity: "+str(hum)+"  Temperature: "+str(temp)+"  Pressure: "+str(prsr))
             try:
-                url="http://api.thingspeak.com/update?api_key=[write api key]&field1="+str(temp)+"&field2="+str(hum)+"&field3=100"
+                url="http://api.thingspeak.com/update?api_key=[write api key]&field1="+str(temp)+"&field2="+str(hum)+"&field3=1"+str(prsr)
                 f=urllib2.urlopen(url)
                 print(f.read())
                 f.close()
             except:
-                  print("error uploading data")
+                  print("Error uploading data")
                   pass
             sleep(10)
       except:
