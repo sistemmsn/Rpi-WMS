743c955b7b7e95d5995bb2a7fd002e64ac33f16d
Yaswant Narayan <yoyaswant@gmail.com>
2018-03-17 10:31:59 +0530

    files rearranged

--- Rpi/wms.py
+++ Rpi/wms.py
@@ -1,11 +1,13 @@
 import Adafruit_DHT
-from time import sleep
 import Adafruit_BMP.BMP085 as BMP085
+
+from time import sleep
 import requests
+import python-firebase
 
 while True:
       try:
-            hum, temp = Adafruit_DHT.read_retry(11, 14)#DHT_11->11 and 8th pin BCM14
+            hum, temp = Adafruit_DHT.read_retry(11, 27)#DHT_11->11 and 27th GPIO pin
             sensor = BMP085.BMP085()
             prsr=sensor.read_pressure()
             requests.post("http://api.thingspeak.com/update?api_key=[write api key]&field1="+str(temp)+"&field2="+str(hum)+"&field3=1"+str(prsr))
